15c8ebcaa5e08d12f44062b3186a93aa82358fb7
brurib <bruno.zanchettin@gmail.com>
2020-09-13 23:33:07 +0200

    first commit, simple login test

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert';
+
+const SERVER_IP = 'https://n1lv4wjyc3.execute-api.eu-west-1.amazonaws.com/dev';
+final storage = FlutterSecureStorage();
+
+void main() {
+  runApp(MyApp());
+}
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void displayDialog(context, title, text) => showDialog(
+        context: context,
+        builder: (context) =>
+            AlertDialog(title: Text(title), content: Text(text)),
+      );
+
+  Future<String> attemptLogIn(String username, String password) async {
+    var res = await http
+        .post("$SERVER_IP/login", headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      'x-api-key': 'xddfuheeLr5ne39P10y4z8pUx6unUweP8xxKjOe5'
+    },
+     body: jsonEncode(<String, String>{
+          'username': username,
+          'password': password
+        }))
+        .catchError((error) {
+      print(error.toString());
+    });
+    if (res.statusCode == 200) return res.body;
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Log In"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'Password'),
+              ),
+              FlatButton(
+                  onPressed: () async {
+                    var username = _usernameController.text;
+                    var password = _passwordController.text;
+                    var jwt = await attemptLogIn(username, password);
+                    if (jwt != null) {
+                      storage.write(key: "jwt", value: jwt);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomePage.fromBase64(jwt)));
+                    } else {
+                      displayDialog(context, "An Error Occurred",
+                          "No account was found matching that username and password");
+                    }
+                  },
+                  child: Text("Log In")),
+            ],
+          ),
+        ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage(this.jwt, this.payload);
+
+  factory HomePage.fromBase64(String jwt) => HomePage(
+      jwt,
+      json.decode(
+          ascii.decode(base64.decode(base64.normalize(jwt.split(".")[1])))));
+
+  final String jwt;
+  final Map<String, dynamic> payload;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: Text("Secret Data Screen")),
+        body: Center(
+          child: FutureBuilder(
+              future:
+                  http.read('$SERVER_IP/data', headers: {"Authorization": jwt}),
+              builder: (context, snapshot) => snapshot.hasData
+                  ? Column(
+                      children: <Widget>[
+                        Text("${payload['username']}, here's the data:"),
+                        Text(snapshot.data,
+                            style: Theme.of(context).textTheme.display1)
+                      ],
+                    )
+                  : snapshot.hasError
+                      ? Text("An error occurred")
+                      : CircularProgressIndicator()),
+        ),
+      );
+}
+
+class MyApp extends StatelessWidget {
+  Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(key: "jwt");
+    if (jwt == null) return "";
+    return jwt;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Authentication Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+          future: jwtOrEmpty,
+          builder: (context, snapshot) {
+            if (snapshot.data != "" && snapshot.hasData) {
+              var str = snapshot.data;
+              var jwt = str.split(".");
+
+              if (jwt.length != 3) {
+                return LoginPage();
+              } else {
+                var payload = json.decode(
+                    ascii.decode(base64.decode(base64.normalize(jwt[1]))));
+                if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000)
+                    .isAfter(DateTime.now())) {
+                  return HomePage(str, payload);
+                } else {
+                  return LoginPage();
+                }
+              }
+            } else {
+              return LoginPage();
+            }
+          }),
+    );
+  }
+}
